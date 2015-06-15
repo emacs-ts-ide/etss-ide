726cb010857124eb119ae847e01c89b2c338c144
Carl Xiong <xiongc05@gmail.com>
2015-06-15 18:48:43 +0800

    Add "jump-to-definition"
    * Also some basic enable/disable routines

--- etss-ide.el
+++ etss-ide.el
@@ -12,11 +12,21 @@
   "Minor mode for TypeScript coding in Emacs."
   :lighter " ETSS"
   :keymap '()
-  (when (eq major-mode 'typescript-mode)
-    (etss-ide--setup-company)
-    (etss-ide--setup-flymake)
-    (etss-setup-current-buffer)
-    (message "ETSS-IDE Loaded!")))
+  (if (eq major-mode 'typescript-mode)
+      (cond
+       (etss-ide-mode
+        (etss-setup-current-buffer)
+        (etss-ide--setup-company)
+        (etss-ide--setup-flymake)
+        (etss-ide--setup-extra-keymap)
+        (message "ETSS-IDE Loaded!"))
+       ;; disable
+       (t
+        (flymake-mode-off)
+        ;; TODO restore keymap
+        ;; TODO clean up various ETSS status for the buffer
+        (message "ETSS-IDE unloaded.")))
+    (error "ETSS-IDE: can't be used for non-TypeScript files.")))
 
 (defun etss-ide--setup-company ()
   (unless (memq 'company-etss company-backends)
@@ -29,7 +39,30 @@
   (unless (and (member flymake-etss--err-line-pattern flymake-err-line-patterns)
                (assoc ".+\\.ts$" flymake-allowed-file-name-masks))
     (flymake-etss-setup))
-  (flymake-mode 1)
+  (flymake-mode-on)
   (message "ETSS-IDE: enable `flymake-etss'."))
+
+(defun etss-ide--setup-extra-keymap ()
+  ;;; TODO how to revert this setup?
+  (define-key typescript-mode-map
+    (kbd "M-.") #'etss-ide--jump-to-definition))
+
+(defun etss-ide--jump-to-definition ()
+  "Jump to definition at point."
+  (interactive)
+  (let ((ret (etss--get-definition))
+        deffile col line)
+    (if (null ret)
+        (message "ETSS: No definition found at point!")
+      (setq deffile (etss-utils/assoc-path ret 'file)
+            line (etss-utils/assoc-path ret '(min line))
+            col (etss-utils/assoc-path ret '(min character)))
+      ;; TODO better integration with `find-tag'?
+      (ring-insert find-tag-marker-ring (point-marker))
+
+      (find-file deffile)
+      (goto-char (point-min))
+      (forward-line (- line 1))
+      (forward-char (- col 1)))))
 
 (provide 'etss-ide)
